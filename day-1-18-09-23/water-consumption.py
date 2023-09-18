aac5e5c0be5993fded56e3b9128e187fea531836
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-18 18:24:25 +0530

    Update water-consumption.py

--- day-1-18-09-23/water-consumption.py
+++ day-1-18-09-23/water-consumption.py
@@ -1,8 +1,12 @@
+'''Recently, Chef visited his doctor. The doctor advised Chef to drink at least 20002000 ml of water each day.
+
+Chef drank XX ml of water today. Determine if Chef followed the doctor's advice or not.'''
 # we need a test case counter first
 T = int(input())
 Y = list()
 while T != 0 :
-    print (T)
+    X = int(input()) # enter the amount of water chef drank
+    Y.append(X) # amount of water chef drank placed in a list Y
     T = T-1
 for i in Y :    #compares each item of list Y with 2000
     if i >= 2000 : print("YES")
