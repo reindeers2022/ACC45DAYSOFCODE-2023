4bde8d3fa350a97ea076440dc636782ffbafd8a6
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-18 18:14:09 +0530

    compares amount of water drunk
    I have added a list that stores the amount of water chef drank and compared it with 2000ml

--- day-1-18-09-23/water-consumption.py
+++ day-1-18-09-23/water-consumption.py
@@ -1,5 +1,9 @@
 # we need a test case counter first
 T = int(input())
+Y = list()
 while T != 0 :
     print (T)
     T = T-1
+for i in Y :    #compares each item of list Y with 2000
+    if i >= 2000 : print("YES")
+    else : print("NO")
