2740c1402f9cd35c2b59a7474ccc85de2fe080fc
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-19 19:31:15 +0530

    fixed calculation bug
    now, using indexes to use items of list within list as variable names X, Y, Z were not giving desired results

--- day-2-19-09-23/buy-choco.py
+++ day-2-19-09-23/buy-choco.py
@@ -3,11 +3,16 @@ chocolates for Chefina where each chocolate costs Z rupees. Find the maximum
 number of chocolates that Chef can buy for Chefina.'''
 T = int(input()) #inputs number of test cases user wishes to run
 myCases = list()
+myAns = list()
 while T != 0 :
     X = input(); Y = input(); Z = input()
     T = T - 1
     myCases.append([X, Y, Z])
-#print (myCases)
+#print(myCases)
 for i in myCases :
-    nChoco = (((5*int(X)) + (10*int(Y)))/int(Z))
-    print (int(nChoco))
+    inr5 = 5*int(i[0])
+    inr10 = 10*int(i[1])
+    (nChoco) = (inr5 + inr10)/int(i[2])
+    myAns.append(int(nChoco))
+print("\n")
+for n in myAns : print(n)
