2fcd4bb7fcbf5657a8140f09254527a301855ae5
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-20 19:05:02 +0530

    Update chair-req.py

--- day-3-20-09-23/chair-req.py
+++ day-3-20-09-23/chair-req.py
@@ -5,23 +5,26 @@ This year X students joined his class and each student will require one chair
 to sit on. Chef already has Y chairs in his class. Determine the minimum number
 of new chairs Chef must buy so that every student is able to get one chair to
 sit on.
+constraints -
+1≤T≤1000
 '''
 T = int(input())
+if (T < 0 or T > 1000) : print("1≤T≤1000")
+else :
+    userInputs = list()
 
-userInputs = list()
+    def xtra_chairs() :
+        for i in userInputs :
+            X = int(i[0])
+            Y = int(i[1])
+            if ((X-Y) < 0) : print(0)   #for differences <0 prints 0
+            else : print(X-Y)
 
-def xtra_chairs() :
-    for i in userInputs :
-        X = int(i[0])
-        Y = int(i[1])
-        if ((X-Y) < 0) : print(0)   #for differences <0 prints 0
-        else : print(X-Y)
+    while (T != 0) :    #runs test cases
+         myInputs = input()
+         item = myInputs.split()
+         userInputs.append(item)    #stores user inputs in a list
+         T = T - 1
 
-while (T != 0) :    #runs test cases
-     myInputs = input()
-     item = myInputs.split()
-     userInputs.append(item)    #stores user inputs in a list
-     T = T - 1
-
-print("\n")
-xtra_chairs()
+    print("\n")
+    xtra_chairs()
