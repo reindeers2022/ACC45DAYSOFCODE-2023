1680c8de63701060019f0ececc05a1ed1b8d8381
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-23 10:56:55 +0530

    storing user inputs
    stored user inputs in a list to later operate upon its values

--- day-6-23-09-23/car-req.py
+++ day-6-23-09-23/car-req.py
@@ -5,6 +5,10 @@ N friends want to go to a restaurant for a party. Find the minimum number of
 cars required to accommodate all the friends.
 '''
 T = int(input())    #inputs number of test cases user wishes to run
+userInputs = list()
 while (T != 0) :
-    print(T)
+    #print(T)
+    N = int(input())
+    userInputs.append(N)    #stores user-inputs into a list
     T = T - 1
+print(userInputs)
