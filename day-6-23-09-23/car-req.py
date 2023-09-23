2f112c65d260fd026372017e6af2caff33b72427
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-23 10:53:01 +0530

    Create car-req.py
    added test cases loop

---
+++ day-6-23-09-23/car-req.py
@@ -0,0 +1,10 @@
+'''
+A single car can accommodate at most 44 people.
+
+N friends want to go to a restaurant for a party. Find the minimum number of
+cars required to accommodate all the friends.
+'''
+T = int(input())    #inputs number of test cases user wishes to run
+while (T != 0) :
+    print(T)
+    T = T - 1
