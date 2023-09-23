da0f1a7d0287d68721ae0bce07402333d94486da
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-23 11:13:06 +0530

    fixed answers output
    used math.ceil() to resolve for divisions resulting in quotients with decimals

--- day-6-23-09-23/car-req.py
+++ day-6-23-09-23/car-req.py
@@ -3,12 +3,25 @@ A single car can accommodate at most 44 people.
 
 N friends want to go to a restaurant for a party. Find the minimum number of
 cars required to accommodate all the friends.
+
+constraints - 
+1≤T≤1000
+2≤N≤1000
 '''
+import math
 T = int(input())    #inputs number of test cases user wishes to run
 userInputs = list()
+def car_req() :
+    for i in userInputs :
+        if (i <= 4) : print(1)
+        elif (i > 4) : print(math.ceil(i/4))    #ceil function from math adds +1
+        #to number of cars required when answer is in decimal
+        else :  print("something went wrong")
 while (T != 0) :
     #print(T)
     N = int(input())
     userInputs.append(N)    #stores user-inputs into a list
     T = T - 1
-print(userInputs)
+#print(userInputs)
+print("\n")
+car_req()   #calls function that prints number of cars required
