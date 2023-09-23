e385689cfc650c8c621f6616127291d6c0498990
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-23 11:22:18 +0530

    added constraints to code
    1≤T≤1000
    2≤N≤1000

--- day-6-23-09-23/car-req.py
+++ day-6-23-09-23/car-req.py
@@ -4,11 +4,11 @@ A single car can accommodate at most 44 people.
 N friends want to go to a restaurant for a party. Find the minimum number of
 cars required to accommodate all the friends.
 
-constraints - 
+constraints -
 1≤T≤1000
 2≤N≤1000
 '''
-import math
+import math, sys
 T = int(input())    #inputs number of test cases user wishes to run
 userInputs = list()
 def car_req() :
@@ -17,11 +17,17 @@ def car_req() :
         elif (i > 4) : print(math.ceil(i/4))    #ceil function from math adds +1
         #to number of cars required when answer is in decimal
         else :  print("something went wrong")
-while (T != 0) :
-    #print(T)
-    N = int(input())
-    userInputs.append(N)    #stores user-inputs into a list
-    T = T - 1
-#print(userInputs)
-print("\n")
-car_req()   #calls function that prints number of cars required
+if (T not in range(1,1001)) : print("1≤T≤1000")
+elif (T in range(1,1001)) :
+    while (T != 0) :
+        N = int(input())
+        T = T - 1
+        if (N in range(2,1001)) :
+            userInputs.append(N)    #stores user-inputs into a list
+        elif (N not in range(2,1001)) :
+            print("2≤N≤1000")
+            sys.exit()
+        else : print("something went wrong")
+    print("\n")
+    car_req()   #calls function that prints number of cars required
+else : print("something went wrong")
