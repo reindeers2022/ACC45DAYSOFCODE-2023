8c8e0867ddd4e35454de71703c08deb3f479262d
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-24 12:13:19 +0530

    Update ig-spam.py
    added following constraints -
    1≤T≤100
    1≤X,Y≤100

--- day-7-24-09-23/ig-spam.py
+++ day-7-24-09-23/ig-spam.py
@@ -4,7 +4,12 @@ account is more than 10 times the count of followers.
 
 Given the following and follower count of an account as X and Y respectively,
 find whether it is a spam account
+
+constraints -
+1≤T≤100
+1≤X,Y≤100
 '''
+import sys
 T = int(input())
 userInputs = list()
 def spam() :
@@ -14,10 +19,17 @@ def spam() :
         if (Y*10 < X) : print("YES")
         elif (Y*10 >= X) : print("NO")
         else : print("something went wrong")
-while (T != 0) :
-    rawInputs = input()
-    formattedInputs = rawInputs.split()
-    userInputs.append(formattedInputs)
-    T = T - 1
-print("\n")
-spam()
+if (T in range(1,101)) :
+    while (T != 0) :
+        rawInputs = input()
+        T = T - 1
+        formattedInputs = rawInputs.split()
+        for h in formattedInputs :
+            if (int(h) not in range(1,101)) :
+                print("1≤X,Y≤100")
+                sys.exit()
+        userInputs.append(formattedInputs)
+    print("\n")
+    spam()
+elif (T not in range(1,101)) : print("1≤T≤100")
+else : print("something went wrong")
