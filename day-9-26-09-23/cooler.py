b5f1fbcfbeb8445b136e6b12ff8c8e4f7a2c50c5
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-26 17:57:55 +0530

    Update cooler.py
    added constraints

--- day-9-26-09-23/cooler.py
+++ day-9-26-09-23/cooler.py
@@ -8,8 +8,12 @@ cooler to keep his room cool. He has two options available:
 Chef wonders what is the maximum number of months for which he can rent the
 cooler such that the cost of renting is strictly less than the cost of
 purchasing it.
+constraints -
+1≤T≤1000
+1≤X,Y≤10e9
 '''
-import math
+import math, sys
+end = pow(10,9)
 T = int(input())    #asks for number of test cases
 userInputs = list() #will store user`s inputs
 def cost() :
@@ -20,12 +24,20 @@ def cost() :
         if (rentCost <= 1) : print(0)
         elif (rentCost > 2) : print(math.floor(rentCost))
         else : print("something went wrong")
-while(T != 0) :
-    #print(T)
-    rawInputs = input()
-    T = T - 1
-    rawInputs = rawInputs.split()
-    userInputs.append(rawInputs)
-print("\n")
-cost()
-#print(userInputs)
+if (T in range(1,1001)) :
+    while(T != 0) :
+        #print(T)
+        rawInputs = input()
+        T = T - 1
+        rawInputs = rawInputs.split()
+        userInputs.append(rawInputs)
+        for h in rawInputs :
+            if (int(h) not in range(1,end)) :
+                print("1≤X,Y≤10e9")
+                sys.exit()
+    print("\n")
+    cost()
+    #print(userInputs)
+elif (T not in range(1,1001)) :
+    print("1≤T≤1000")
+else : print("something went wrong")
