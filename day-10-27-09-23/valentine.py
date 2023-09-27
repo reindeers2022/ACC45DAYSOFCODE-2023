1c26a9c3db1bdf3a68d5dd9093c70aa800109060
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-27 20:15:18 +0530

    added constraints
    1≤T≤1000
    1≤X,Y≤100

--- day-10-27-09-23/valentine.py
+++ day-10-27-09-23/valentine.py
@@ -4,8 +4,11 @@ someone special.
 
 Chef has a total of X rupees and one chocolate costs Y rupees. What is the
 maximum number of chocolates Chef can buy?
+constraints -
+1≤T≤1000
+1≤X,Y≤100
 '''
-import math
+import math, sys
 T = int(input())    #asks for number of test cases to run
 userInputs = list()
 def nchoco() :
@@ -14,11 +17,21 @@ def nchoco() :
         Y = int(i[1])    #cost of one chocolate
         nchocoBuy = math.floor(X/Y)
         print(nchocoBuy)
-while (T != 0) :
-    #print(T)
-    rawInputs = input()
-    rawInputs = rawInputs.split()
-    T = T - 1
-    userInputs.append(rawInputs)
-print("\n")
-nchoco()
+if (T in range(1,1001)) :
+    while (T != 0) :
+        #print(T)
+        rawInputs = input()
+        rawInputs = rawInputs.split()
+        for h in rawInputs :
+            if (int(h) in range(1,101)) : pass
+            elif (int(h) not in range(1,101)) :
+                print("1≤X,Y≤100")
+                sys.exit()
+            else : print("something went wrong")
+        T = T - 1
+        userInputs.append(rawInputs)
+    print("\n")
+    nchoco()
+elif (T not in range(1,1001)) :
+    print("1≤T≤1000")
+else : print("something went wrong")
