aacb92c327e5c91f83d5dbcaba5bcf21abf817c1
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-09-30 11:45:17 +0530

    Create nextgen.py

---
+++ day-13-30-09-23/nextgen.py
@@ -0,0 +1,20 @@
+'''
+Chef is currently working for a secret research group called NEXTGEN. While the
+rest of the world is still in search of a way to utilize Helium-3 as a fuel,
+NEXTGEN scientists have been able to achieve 2 major milestones:
+
+    Finding a way to make a nuclear reactor that will be able to utilize
+    Helium-3 as a fuel
+    Obtaining every bit of Helium-3 from the moon's surface
+
+Moving forward, the project requires some government funding for completion,
+which comes under one condition: to prove its worth, the project should power
+Chefland by generating at least A units of power each year for the next B years.
+
+Help Chef determine whether the group will get funded assuming that the moon has
+X grams of Helium-3 and 1 gram of Helium-3 can provide Y units of power.
+'''
+T = int(input())
+while(T != 0) :
+    #print(T)
+    T = T - 1
