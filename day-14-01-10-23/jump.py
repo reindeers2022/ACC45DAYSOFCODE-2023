be35b2407cd49e3c203ebc9b3d3280d5b5048ff5
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-10-01 18:00:36 +0530

    Update jump.py

--- day-14-01-10-23/jump.py
+++ day-14-01-10-23/jump.py
@@ -4,7 +4,10 @@ Chef is currently standing at stair 0 and he wants to reach stair numbered X.
 Chef can climb either Y steps or 1 step in one move.
 Find the minimum number of moves required by him to reach exactly the stair
 numbered X.
+1≤T≤500
+1≤X,Y≤100
 '''
+import sys
 T = int(input())
 usrInputs = list()
 def jump() :
@@ -14,12 +17,19 @@ def jump() :
         if (X%Y == 0) : print(int(X/Y))
         elif (X%Y > 0) : print(int(X/Y) + int(X%Y))
         else : print("something went wrong...")
-while(T != 0) :
-    #print(T)
-    rawInputs = input()
-    rawInputs = rawInputs.split()
-    T = T - 1
-    usrInputs.append(rawInputs)
-#print(usrInputs)
-print("\n")
-jump()
+if (T in range(1,501)) :
+    while(T != 0) :
+        #print(T)
+        rawInputs = input()
+        rawInputs = rawInputs.split()
+        for h in rawInputs :
+            if (int(h) not in range(1,101)) :
+                print("1≤X,Y≤100")
+                sys.exit()
+        T = T - 1
+        usrInputs.append(rawInputs)
+    #print(usrInputs)
+    print("\n")
+    jump()
+elif (T not in range(1,501)) : print("1≤T≤500")
+else : print("something went wrong...")
