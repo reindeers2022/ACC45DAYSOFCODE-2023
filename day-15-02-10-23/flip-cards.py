df8e0b6cadf332c02378146ee8bc2af3a1fcad32
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-10-02 12:38:25 +0530

    added cosntraints
    1≤T≤5000
    2≤N≤100
    0≤X≤N

--- day-15-02-10-23/flip-cards.py
+++ day-15-02-10-23/flip-cards.py
@@ -9,7 +9,12 @@ In one operation, we can do the following:
 
 What is the minimum number of operations we must perform so that all the cards
 face in the same direction (i.e. either all are face-up or all are face-down)?
+constraints -
+1≤T≤5000
+2≤N≤100
+0≤X≤N
 '''
+import sys
 T = int(input())
 usrInput = list()
 def minFlip() :
@@ -21,11 +26,21 @@ def minFlip() :
         if (moveFlipUp > moveFlipDown) : print(moveFlipDown)
         elif (moveFlipUp <= moveFlipDown) : print(moveFlipUp)
         else : print("something went wrong...")
-while (T != 0) :
-    rawInputs = input()
-    T = T - 1
-    rawInputs = rawInputs.split()
-    usrInput.append(rawInputs)
-#print(usrInput)
-print("\n")
-minFlip()
+if (T in range(1,5001)) :
+    while (T != 0) :
+        rawInputs = input()
+        T = T - 1
+        rawInputs = rawInputs.split()
+        if (int(rawInputs[0]) not in range(2,101)) :
+                print("2≤N≤100")
+                sys.exit()
+        if (int(rawInputs[1]) not in range(0,int(rawInputs[0])+1)) :
+                print("0≤X≤N")
+                sys.exit()
+        usrInput.append(rawInputs)
+    #print(usrInput)
+    print("\n")
+    minFlip()
+elif (T not in range(1,5001)) :
+    print("1≤T≤5000")
+else : print("something went wrong...")
