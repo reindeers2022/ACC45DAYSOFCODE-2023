dcd391d70070b650acb10fbe47be8163939ffc2c
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-10-05 19:35:58 +0530

    program to know number of spaces, \n for given text

---
+++ day-18-05-10-23/count-spaces-newline-tabs.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*PROGRAM TO CALCULATE THE TOTAL NUMBERS OF TABS, BLANKS, NEWLINES OF GIVEN TEXT*/
+int main(void)
+{
+  int blanks_nr = 0;
+  int tabs_nr = 0;
+  int newlines_nr = 0;
+
+  char c;
+  printf("PRESS CTRL+D (LINUX) AND CTRL+Z (WINDOWS) TO STOP WRITING\nINPUT PARAGRAPHS INLCUDING CHARACTERS, NUMBERS, SPACES ETC....\n");
+  while ((c = getchar()) != EOF)
+  {
+    if (c == ' ')
+    {
+      ++blanks_nr;
+    }
+    else if (c == '\t')
+    {
+      ++tabs_nr;
+    }
+    else if (c == '\n')
+    {
+      ++newlines_nr;
+    }
+  }
+
+  printf("\nblanks_nr: %d\ntabs_nr: %d\nnewlines_nr: %d\n",
+         blanks_nr, tabs_nr, newlines_nr);
+
+  return 0;
+}
