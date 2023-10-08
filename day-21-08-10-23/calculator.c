7ef62d89fcba97d2349da24c282a11337acea610
SHUBHANG SINGH TOMAR <tomarshubhang@gmail.com>
2023-10-08 17:06:23 +0530

    create calculator.c

---
+++ day-21-08-10-23/calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  int a, b, opt;
+
+  printf("\tTWO OPERAND CALCULATOR\nENTER FIRST NUMBER :\n");
+  scanf("%d", &a);
+  printf("ENTER SECOND NUMBER (must be lesser than first number) :\n");
+  scanf("%d", &b);
+
+  printf("PRESS 1 TO ADD\nPRESS 2 TO SUBTRACT\n");
+  printf("PRESS 3 TO MULTIPLY\nPRESS 4 TO DIVIDE\nPRESS 5 TO GET REMAINDER OF DIVISION\n");
+  scanf("%d", &opt);
+
+  if (opt == 1) {printf("Sum is : %d", a+b);}
+  else if (opt == 2) {printf("Difference is : %d", a-b);}
+  else if (opt == 3) {printf("Product is : %d", a*b);}
+  else if (opt == 4) {printf("Quotient is : %d", a/b);}
+  else if (opt == 5) {printf("Remainder is : %d", a%b);}
+  else {printf("something went wrong...");}
+  return 0;
+}
