b7f8474c35b27d21394a5a4522cc8ce0dc94e4d7
shubhang <tomarshubhang@gmail.com>
2023-10-14 18:48:20 +0530

    used math.h to eavaluate compound interest

--- day-27-14-10-23/compound_interest.c
+++ day-27-14-10-23/compound_interest.c
@@ -11,11 +11,13 @@ t = time
 n = times interest is compounded per year
 */
 #include <stdio.h>
+#include <math.h>
 int main() {
   /*
   firstly i must take the necessary inputs from the user
   and store them as float datatype
   */
+
   float cpdInt, Prn, r, n, t;
   printf("enter principal amount :\n");
   scanf("%f", &Prn);
@@ -25,6 +27,12 @@ int main() {
   scanf("%f", &n);
   printf("enter time :\n");
   scanf("%f", &t);
-  
+
+  /*
+  to calculate the compound interest, we need to raise a term to some
+  power so we use pow() from <math.h>
+  */
+  float x = pow((1+(r/n)),(n*t));
+  printf("compound interest : %.4f\n", Prn*x);
   return 0;
 }
