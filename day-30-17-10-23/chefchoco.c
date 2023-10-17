13aaf8d0d465e47dbffc4729f0b80ad3f855c5d9
shubhang <tomarshubhang@gmail.com>
2023-10-17 19:24:19 +0530

    added constraints and comments

--- day-30-17-10-23/chefchoco.c
+++ day-30-17-10-23/chefchoco.c
@@ -5,16 +5,39 @@ The cost of 1 chocolate is Y rupees.
 
 Find the minimum money in rupees Chef needs to
 spend so that he can gift C chocolates to Botswal.
+
+CONSTRAINTS -
+1≤T≤100
+1≤C≤100
+0≤X≤C
+1≤Y≤100
 */
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
   int t, c, x, y, i = 0, ans[101], more_Choco, money_req;
   scanf("%d", &t);
+  if (t < 1 || t > 100) {
+    printf("1≤T≤100\n");
+    exit(0);
+  }
   while (i != t) {
     scanf("%d %d %d", &c, &x, &y);
+    if (c < 1 || c > 100) {
+      printf("1≤C≤100\n");
+      exit(0);
+    }
+    else if (y < 1 || y > 100) {
+      printf("1≤Y≤100\n");
+      exit(0);
+    }
+    else if (x < 0 || x > c) {
+      printf("0≤X≤C\n");
+      exit(0);
+    }
     more_Choco = c - x;
     money_req = y * more_Choco;
-    ans[i] = money_req;
+    ans[i] = money_req; //stores amount of money required in an array
     i = i + 1;
   }
   printf("\n");
