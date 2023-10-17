da08d178cd216e5688abac183c3ca2da95eb7d25
shubhang <tomarshubhang@gmail.com>
2023-10-17 19:15:59 +0530

    added arithmetic calculations required

--- day-30-17-10-23/chefchoco.c
+++ day-30-17-10-23/chefchoco.c
@@ -8,11 +8,18 @@ spend so that he can gift C chocolates to Botswal.
 */
 #include <stdio.h>
 int main() {
-  int t;
+  int t, c, x, y, i = 0, ans[101], more_Choco, money_req;
   scanf("%d", &t);
-  while (t != 0) {
-    printf("%d\n", t);
-    t = t - 1;
+  while (i != t) {
+    scanf("%d %d %d", &c, &x, &y);
+    more_Choco = c - x;
+    money_req = y * more_Choco;
+    ans[i] = money_req;
+    i = i + 1;
+  }
+  printf("\n");
+  for (i = 0; i != t; i = i + 1) {
+    printf("%d\n", ans[i]);
   }
   return 0;
 }
