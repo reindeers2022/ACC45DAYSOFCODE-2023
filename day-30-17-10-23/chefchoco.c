ae51f8c9bfee97029a5548d01353391dc08315f7
shubhang <tomarshubhang@gmail.com>
2023-10-17 18:51:10 +0530

    test case counter

---
+++ day-30-17-10-23/chefchoco.c
@@ -0,0 +1,18 @@
+/*
+Chef wants to gift C chocolates to Botswal on
+his birthday. However, he has only X chocolates with him.
+The cost of 1 chocolate is Y rupees.
+
+Find the minimum money in rupees Chef needs to
+spend so that he can gift C chocolates to Botswal.
+*/
+#include <stdio.h>
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t != 0) {
+    printf("%d\n", t);
+    t = t - 1;
+  }
+  return 0;
+}
